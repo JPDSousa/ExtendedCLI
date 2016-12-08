15bd0c52313d32bc961290073b3b5474eaad5690
Joao Sousa <jpd.sousa@campus.fct.unl.pt>
2016-12-08 17:15:15 +0000

    Added equals and hashcode to argument

--- src/org/extendedCLI/main/argument/DefaultArgument.java
+++ src/org/extendedCLI/main/argument/DefaultArgument.java
@@ -129,4 +129,34 @@ class DefaultArgument implements Argument {
 		return builder.build(); 
 	}
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (getClass() != obj.getClass()) {
+			return false;
+		}
+		DefaultArgument other = (DefaultArgument) obj;
+		if (name == null) {
+			if (other.name != null) {
+				return false;
+			}
+		} else if (!name.equals(other.name)) {
+			return false;
+		}
+		return true;
+	}
+
 }
