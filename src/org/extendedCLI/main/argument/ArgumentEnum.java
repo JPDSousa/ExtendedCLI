1fe9e667e550212404cdc6a61dbbe56df7a9f0cb
Joao Sousa <jpd.sousa@campus.fct.unl.pt>
2017-03-09 10:58:44 +0000

    Added documentation for ArgumentEnum

--- src/org/extendedCLI/main/argument/ArgumentEnum.java
+++ src/org/extendedCLI/main/argument/ArgumentEnum.java
@@ -1,10 +1,29 @@
 package org.extendedCLI.main.argument;
 
-@SuppressWarnings("javadoc")
+/**
+ * This interface is used as a wrapper for enumerations ({@link Enum}. For code readability and simplicity, we design this interface so that developers
+ * can use <b>enums</b> to represent arguments.
+ * 
+ * <p> Furthermore, this interface provides the concept of group. A group represents a subset of arguments in which only one argument per group is allowed
+ * in the same instruction.
+ * 
+ * @author Joao
+ *
+ */
 public interface ArgumentEnum {
 	
+	/**
+	 * Returns the argument wrapped by this interface.
+	 * 
+	 * @return argument wrapped by this interface.
+	 */
 	Argument getArgument();
 	
+	/**
+	 * Returns the group id of the argument wrapped by this interface.
+	 * 
+	 * @return group id of the argument wrapped by this interface.
+	 */
 	int getGroupID();
 
 }
