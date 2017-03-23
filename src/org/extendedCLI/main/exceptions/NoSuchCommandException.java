4641612c681a4b05409cb6482c09c27074d770c9
Joao Sousa <jpd.sousa@campus.fct.unl.pt>
2017-03-23 11:35:37 +0000

    Removed dependencies from ExceptThis project

--- src/org/extendedCLI/main/exceptions/NoSuchCommandException.java
+++ src/org/extendedCLI/main/exceptions/NoSuchCommandException.java
@@ -1,14 +1,12 @@
 package org.extendedCLI.main.exceptions;
 
-import exceptthis.core.AbsCustomException;
-
 @SuppressWarnings("javadoc")
-public final class NoSuchCommandException extends AbsCustomException {
+public final class NoSuchCommandException extends Exception {
 
 	private static final long serialVersionUID = 1L;
 
 	public NoSuchCommandException(String commandName) {
-		super("Command Error.", "Command sintax for command %s is invalid.", commandName);
+		super("Command sintax for command " + commandName + "%s is invalid.");
 	}
 
 }
