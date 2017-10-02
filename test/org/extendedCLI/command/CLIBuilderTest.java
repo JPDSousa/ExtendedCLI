7d0af5e0e03f292fc5df051f5c52768949e7fe56
TunedMidja <tunedmidja@gmail.com>
2017-10-02 22:20:29 +0200

    Command output tests.

--- test/org/extendedCLI/command/CLIBuilderTest.java
+++ test/org/extendedCLI/command/CLIBuilderTest.java
@@ -6,11 +6,14 @@ import static org.mockito.Mockito.*;
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintStream;
 import org.extendedCLI.argument.Arguments;
 import org.extendedCLI.command.standard.Exit;
 import org.extendedCLI.command.standard.Help;
 import org.extendedCLI.command.standard.History;
 import org.extendedCLI.ioAdapters.InputAdapter;
+import org.extendedCLI.ioAdapters.OutputAdapter;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -86,5 +89,44 @@ public class CLIBuilderTest {
     cliBuilder.registerCommand("MOCK_COMMAND", mockCommand);
 
     verify(mockCommand).setInputAdapter(any());
+  }
+
+  @Test
+  public void testSetOutputAdapter() {
+    InputAdapter mockInputAdapter = mock(InputAdapter.class);
+    OutputAdapter mockOutputAdapter = mock(OutputAdapter.class);
+    AbstractCommand mockCommand = mock(AbstractCommand.class);
+
+    cliBuilder.setInput(mockInputAdapter);
+    cliBuilder.setOutput(mockOutputAdapter);
+    cliBuilder.registerCommand("MOCK_COMMAND", mockCommand);
+
+    verify(mockCommand).setOutputAdapter(mockOutputAdapter);
+  }
+
+  @Test
+  public void testSetOutputStream() {
+    InputAdapter mockInputAdapter = mock(InputAdapter.class);
+    OutputStream mockOutputStream = mock(OutputStream.class);
+    AbstractCommand mockCommand = mock(AbstractCommand.class);
+
+    cliBuilder.setInput(mockInputAdapter);
+    cliBuilder.setOutput(mockOutputStream);
+    cliBuilder.registerCommand("MOCK_COMMAND", mockCommand);
+
+    verify(mockCommand).setOutputAdapter(any());
+  }
+
+  @Test
+  public void testSetOutputPrintStream() {
+    InputAdapter mockInputAdapter = mock(InputAdapter.class);
+    PrintStream mockOutputPrintStream = mock(PrintStream.class);
+    AbstractCommand mockCommand = mock(AbstractCommand.class);
+
+    cliBuilder.setInput(mockInputAdapter);
+    cliBuilder.setOutput(mockOutputPrintStream);
+    cliBuilder.registerCommand("MOCK_COMMAND", mockCommand);
+
+    verify(mockCommand).setOutputAdapter(any());
   }
 }
